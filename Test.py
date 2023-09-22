1afb05dd1a617ee1e0d0ae643c4a9348642e8c48
dsste <derk.stegemangarnica@student.hu.nl>
2023-09-22 15:11:09 +0200

    initial commit

--- Test.py
+++ Test.py
@@ -1,2 +1,9 @@
-#hallo
-#hallo#bye
+#Zuil
+naam = input("Wat is uw naam? ")
+if not naam:
+    naam = "Anoniem"
+print(f"Hallo {naam}! Wij zijn erg benieuwd naar uw mening over ons. U kunt uw opmerkingen hieronder invoeren.")
+bericht = input("Voer uw bericht in (maximaal 140 karakters: ")
+
+
+
