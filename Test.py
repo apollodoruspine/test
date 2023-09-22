a521d38eb68094cd63931c1b25178dc326a664cd
dsste <derk.stegemangarnica@student.hu.nl>
2023-09-22 16:18:54 +0200

    initial commit

--- Test.py
+++ Test.py
@@ -3,7 +3,20 @@ naam = input("Wat is uw naam? ")
 if not naam:
     naam = "Anoniem"
 print(f"Hallo {naam}! Wij zijn erg benieuwd naar uw mening over ons. U kunt uw opmerkingen hieronder invoeren.")
-bericht = input("Voer uw bericht in (maximaal 140 karakters: ")
+
+HoeveelheidKarakters = 140
+while True:
+    bericht = input("Voer uw bericht in (maximaal 140 karakters): ")
+    if len(bericht) <= HoeveelheidKarakters:
+        print("Bedankt voor het invullen van uw bericht!")
+        break
+    else: print("Uw bericht is te lang. Probeer het opnieuw.")
+
+
+
+
+
+
 
 
 
